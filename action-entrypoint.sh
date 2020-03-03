8711ee7e0d9c36749fa75ee272a033d338470e1f
Daniel Granerud <42067409+NullableInt@users.noreply.github.com>
2020-03-03 10:22:34 +0100

    Update action-entrypoint.sh

--- action-entrypoint.sh
+++ action-entrypoint.sh
@@ -51,6 +51,7 @@ function setup_build_repo {
     # Update local repo with the necessary branch's (shallow) history.
     git fetch
     git checkout -B "$gh_pages_publishing_source" origin/"$gh_pages_publishing_source"
+    git status
 
     # Copy the Git repository there.
     cp -R .git "$(getBuildDir)"
